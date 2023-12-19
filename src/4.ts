21a54fffc44b10c5ab70cdf36f418a0085e823e2
Mykola <boplio.kolyan55@gmail.com>
2023-12-19 23:48:31 +0200

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,52 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person came in.");
+    } else {
+      console.log("Door is closed.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door is open");
+    } else {
+      console.log("Wrong house. Door closed");
+    }
+  }
+}
+
+const key = new Key(Math.random());
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +54,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
